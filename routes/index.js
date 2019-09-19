510f72abcf21fda7d57ebb7c25295933e6fc5b87
YousufQadri <s.yousuf@live.com>
2019-09-19 21:57:57 +0500

    Tab with icons

--- routes/index.js
+++ routes/index.js
@@ -38,7 +38,32 @@ const TabNavigator = createBottomTabNavigator(
       screen: Contact
     }
   },
-  {}
+  {
+    defaultNavigationOptions: ({ navigation }) => ({
+      tabBarIcon: ({ focused, horizontal, tintColor }) => {
+        const { routeName } = navigation.state;
+        let IconComponent = Ionicons;
+        let iconName;
+        if (routeName === "Home") {
+          iconName = "md-home";
+          // Sometimes we want to add badges to some icons.
+          // You can check the implementation below.
+          // IconComponent = HomeIconWithBadge;
+        } else if (routeName === "About") {
+          iconName = `ios-options`;
+        } else if (routeName === "Contact") {
+          iconName = "md-call";
+        }
+
+        // You can return any component that you like here!
+        return <IconComponent name={iconName} size={25} color={tintColor} />;
+      }
+    }),
+    tabBarOptions: {
+      activeTintColor: "coral",
+      inactiveTintColor: "gray"
+    }
+  }
 );
 
 export default createAppContainer(TabNavigator);
